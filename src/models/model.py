576f310a695835012a43642ce5fc4dae7a4b7161
rabernardino <rabernardino@gmail.com>
2025-09-04 21:10:34 -0300

    Starting the mlflow tracking

--- src/models/model.py
+++ src/models/model.py
@@ -4,7 +4,14 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 import numpy as np
+from dotenv import load_dotenv
+import mlflow
+import os
 
+#Loading environment variables
+load_dotenv()
+
+uri = os.getenv('uri')
 
 #Importing data
 raw_data = pd.read_excel('../../data/raw_data.xlsx')
@@ -47,7 +54,28 @@ model = pipeline.Pipeline(
 )
 
 #Fitting the model
-model.fit(X_train, y_train)
+mlflow.set_tracking_uri(uri)
+mlflow.set_experiment(experiment_id=1)
+
 
 #Making the predictions using the trainning set
-y_train_predict = model.predict(X_train)
+
+with mlflow.start_run():
+
+    mlflow.sklearn.autolog()
+
+    model.fit(X_train, y_train)
+    y_train_predict = model.predict(X_train)
+    y_test_predict = model.predict(X_test)
+
+    results_r2_score_train = r2_score(y_train, y_train_predict)
+    results_r2_score_test = r2_score(y_test, y_test_predict)
+
+    results_mean_sq_error_train = mean_absolute_error(y_train, y_train_predict)
+    results_mean_sq_error_test = mean_absolute_error(y_test, y_test_predict)
+
+    mlflow.log_metric("r2_Train", results_r2_score_train)
+    mlflow.log_metric("r2_Test", results_r2_score_test)
+    mlflow.log_metric("MAE_Train", results_mean_sq_error_train)
+    mlflow.log_metric("MAE_Test", results_mean_sq_error_test)
+
